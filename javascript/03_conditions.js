f58b9d63fad93c5d3b09ab04a6d86e816e19a65f
alex <adesmarres@gmail.com>
2021-07-20 17:10:19 +0200

    ajout 2e commit

--- javascript/03_conditions.js
+++ javascript/03_conditions.js
@@ -1,3 +1,7 @@
+
+// Condition if / else if / else
+
+/* 
 let niveauDeSurf = prompt('Saisir votre niveau de surf de 1 a 10');
 
 
@@ -10,3 +14,46 @@ else if( niveauDeSurf <=8){
 else{
     alert('Vous pouvez utiliser un ShortBoard');
 }
+
+*/
+
+// condition SWITCH
+
+
+let typeDePlanche = prompt('Quel marque de plache désirez vous ? Notox, Pukass, Tork') .toLowerCase();
+
+
+console.log(typeDePlanche);
+
+let tailleDeVague = prompt('Quel taille de vagues shouaitez vous surfer ? en Centimetre');
+
+
+
+const TAILLEDEPLANCHE = {
+    pukass: 0.4,
+    notox: 0.8,
+    tork: 1
+};
+
+
+switch (typeDePlanche){
+    case 'tork':{
+        alert('Nous vous conseillons une planche de : ' + TAILLEDEPLANCHE.tork * tailleDeVague + 'pied');
+        break;
+    }
+
+    
+    case 'notox':{
+        alert('Nous vous conseillons une planche de :' + TAILLEDEPLANCHE.notox * tailleDeVague + 'pied');
+        break;
+    }
+
+    case 'pukass':{
+        alert('Nous vous conseillons une planche de :' + TAILLEDEPLANCHE.pukass * tailleDeVague + 'pied');
+        break;
+    }
+
+    default:
+        alert('Veuillez choisir la marque de planche désiré !!!');
+}
+
